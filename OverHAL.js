ada18fd37108632d6b2236896330fe396326508a
Olivier Troccaz <olivier.troccaz@univ-rennes1.fr>
2020-07-08 13:37:08 +0200

    Update OverHAL.js

--- OverHAL.js
+++ OverHAL.js
@@ -1,29 +1,29 @@
 function majReqHAL() {
-  //reqHAL = document.getElementById("reqHAL").value;
-  team = document.getElementById("team").value;
-  year1 = document.getElementById("year1").value;
-  year2 = document.getElementById("year2").value;
-  iann = year1;
-  year = "";
+  //var reqHAL = document.getElementById("reqHAL").value;
+  var team = document.getElementById("team").value;
+  var year1 = document.getElementById("year1").value;
+  var year2 = document.getElementById("year2").value;
+  var iann = year1;
+  var year = "";
   while (iann <= year2) {
     if (iann == year1) {year += "&fq=(";}else{year += "%20OR%20";}
     year += 'producedDateY_i:"'+iann+'"';
     iann++;
   }
   year += ")";
-  txtint = "";
-  txtintplus = "";
+  var txtint = "";
+  var txtintplus = "";
   if(document.getElementById("txtint").checked == true){
     txtint = "%20AND%20(submitType_s:file%20OR%20arxivId_s:?*%20OR%20pubmedcentralId_s:?*)";
     txtintplus = ",arxivId_s,pubmedcentralId_s";
   }
-  aparai = "";
+  var aparai = "";
   if(document.getElementById("aparai").checked == true){
     aparai = "";
   }else{
     aparai = "%20AND%20NOT%20inPress_bool:%22true%22";
   }
-  hal = "https://api.archives-ouvertes.fr/search/?q=collCode_s:";
+  var hal = "https://api.archives-ouvertes.fr/search/?q=collCode_s:";
   hal += '\"'+team+'\"';
   //hal += "&fq=";
   hal += year;
