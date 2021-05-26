feda22f7d61fd69c1209b5d18223df55427d2edf
Olivier Troccaz <olivier.troccaz@univ-rennes1.fr>
2021-05-26 09:25:24 +0200

    Update OverHAL.php

--- OverHAL.php
+++ OverHAL.php
@@ -141,7 +141,7 @@ if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
                                         </p>
 																				
 																				<p class="mb-4">
-                                            Contacts : <a target='_blank' rel='noopener noreferrer' href="https://openaccess.univ-rennes1.fr/interlocuteurs/laurent-jonchere">Laurent Jonchère</a> (Université de Rennes 1) / <a target='_blank' rel='noopener noreferrer' href="https://ecobio.univ-rennes1.fr/personnel.php?qui=Olivier_Troccaz">Olivier Troccaz</a> (CNRS ECOBIO/OSUR).
+                                            Contacts : <a target='_blank' rel='noopener noreferrer' href="https://openaccess.univ-rennes1.fr/interlocuteurs/laurent-jonchere">Laurent Jonchère</a> (Université de Rennes 1) / <a target='_blank' rel='noopener noreferrer' href="https://openaccess.univ-rennes1.fr/interlocuteurs/olivier-troccaz">Olivier Troccaz</a> (CNRS ECOBIO/OSUR).
                                         </p>
 
                                     </div> <!-- end card-body-->
