33d02d7a2c63c8edfdcdc846f31316cdee0edbe3
Olivier Troccaz <olivier.troccaz@univ-rennes1.fr>
2026-04-10 15:50:15 +0200

    Update OverHAL.js

--- OverHAL.js
+++ OverHAL.js
@@ -20,23 +20,23 @@ function majReqHAL() {
     iann++;
   }
   year += ")";
-  var txtint = "";
-  var txtintplus = "";
-  if(document.getElementById("txtint").checked == true){
-    txtint = "%20AND%20NOT%20docType_s:UNDEFINED%20AND%20(submitType_s:file%20OR%20arxivId_s:?*)";
-    txtintplus = ",arxivId_s";
-  }
   if(document.getElementById("aparai").checked == true){
     var aparai = "%20OR%20inPress_bool:%22true%22";
   }else{
     var aparai = "%20AND%20NOT%20inPress_bool:%22true%22";
+  }
+  var txtint = "";
+  var txtintplus = "";
+  if(document.getElementById("txtint").checked == true){
+    txtint = "%20AND%20NOT%20docType_s:UNDEFINED"+aparai+"%20AND%20(submitType_s:file%20OR%20arxivId_s:?*)";
+    txtintplus = ",arxivId_s";
   }
   var hal = "https://api.archives-ouvertes.fr/search/?q=collCode_s:";
   hal += '\"'+team+'\"';
   //hal += "&fq=";
   hal += year;
   hal += txtint;
-  hal += aparai;
+  //hal += aparai;
   hal += "&rows=10000&fl=docType_s,docid,halId_s,authFullName_s,title_s,subTitle_s,journalTitle_s,volume_s,issue_s,page_s,producedDateY_i,proceedings_s,files_s,label_s,citationFull_s,bookTitle_s,doiId_s,conferenceStartDateY_i,publisherLink_s,seeAlso_s";
   hal += txtintplus;
   document.getElementById("reqHAL").value = hal;
